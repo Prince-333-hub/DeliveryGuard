f7b9fb1690e3d703cdb36adbe01ec16f7ebd4301
Prince-333-hub <jyotiradityapati111@gmail.com>
2026-03-26 21:28:41 +0530

    Added App.js - Main file for Phase 2

--- src/App.js
+++ src/App.js
@@ -1,1 +1,20 @@
+import React from 'react';
+import { NavigationContainer } from '@react-navigation/native';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
+import RegistrationScreen from './src/screens/RegistrationScreen';
 
+const Stack = createNativeStackNavigator();
+
+export default function App() {
+  return (
+    <NavigationContainer>
+      <Stack.Navigator initialRouteName="Registration">
+        <Stack.Screen 
+          name="Registration" 
+          component={RegistrationScreen} 
+          options={{ title: 'DeliveryGuard - Register' }} 
+        />
+      </Stack.Navigator>
+    </NavigationContainer>
+  );
+}
